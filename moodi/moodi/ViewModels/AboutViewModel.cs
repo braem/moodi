95826174cce9e7df373c19a0967e905f431ba309
brae <brae@momentum-mod.org>
2021-07-29 04:43:37 -0700

    feat: add button to license in about
    just link them to the github repo

--- moodi/moodi/ViewModels/AboutViewModel.cs
+++ moodi/moodi/ViewModels/AboutViewModel.cs
@@ -7,12 +7,14 @@ namespace moodi.ViewModels
     public class AboutViewModel : BaseViewModel
     {
         public Command OpenGitHubRepo { get; }
+        public Command OpenLicense { get; }
 
         public AboutViewModel()
         {
             Title = "About";
 
             OpenGitHubRepo = new Command(async () => await Browser.OpenAsync("https://github.com/braem/moodi"));
+            OpenLicense = new Command(async () => await Browser.OpenAsync("https://github.com/braem/moodi/blob/master/LICENSE"));
         }
     }
 }
