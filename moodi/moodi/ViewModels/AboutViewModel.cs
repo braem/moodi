cb532616ff2a606c387c3f4042bbf5f75709f343
brae <brae@momentum-mod.org>
2021-08-09 19:50:43 -0700

    chore: add version to about page

--- moodi/moodi/ViewModels/AboutViewModel.cs
+++ moodi/moodi/ViewModels/AboutViewModel.cs
@@ -6,6 +6,12 @@ namespace moodi.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {
+        private string _appAboutTitle = string.Empty;
+        public string AppAboutTitle
+        {
+            get { return _appAboutTitle; }
+            set { SetProperty(ref _appAboutTitle, value); }
+        }
         public Command OpenGitHubRepo { get; }
         public Command OpenLicense { get; }
         public Command OpenThirdPartyPage { get; }
@@ -13,6 +19,7 @@ namespace moodi.ViewModels
         public AboutViewModel()
         {
             Title = "About";
+            AppAboutTitle = "moodi v" + VersionTracking.CurrentVersion;
 
             OpenGitHubRepo = new Command(async () => await Browser.OpenAsync("https://github.com/braem/moodi"));
             OpenLicense = new Command(async () => await Browser.OpenAsync("https://github.com/braem/moodi/blob/master/LICENSE"));
