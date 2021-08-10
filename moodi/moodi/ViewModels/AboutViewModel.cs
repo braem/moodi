95b07872d7291788160c67eb3478dd2a20fb62e3
brae <brae@momentum-mod.org>
2021-08-10 02:10:50 -0700

    feat: beef up about page

--- moodi/moodi/ViewModels/AboutViewModel.cs
+++ moodi/moodi/ViewModels/AboutViewModel.cs
@@ -6,12 +6,28 @@ namespace moodi.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {
-        private string _appAboutTitle = string.Empty;
-        public string AppAboutTitle
+        private string _currentVersion = string.Empty;
+        private string _license = string.Empty;
+        private string _author = string.Empty;
+
+        public string CurrentVersion
         {
-            get { return _appAboutTitle; }
-            set { SetProperty(ref _appAboutTitle, value); }
+            get { return _currentVersion; }
+            set { SetProperty(ref _currentVersion, value); }
         }
+        public string License
+        {
+            get { return _license; }
+            set { SetProperty(ref _license, value); }
+        }
+        public string Author
+        {
+            get { return _author; }
+            set { SetProperty(ref _author, value); }
+        }
+
+        public Command OpenGHProfile { get; }
+        public Command OpenTwitterProfile { get; }
         public Command OpenGitHubRepo { get; }
         public Command OpenLicense { get; }
         public Command OpenThirdPartyPage { get; }
@@ -19,11 +35,15 @@ namespace moodi.ViewModels
         public AboutViewModel()
         {
             Title = "About";
-            AppAboutTitle = "moodi v" + VersionTracking.CurrentVersion;
+            Author = "Brae Stoltz";
+            CurrentVersion = VersionTracking.CurrentVersion + " Alpha";
+            License = "MIT License";
 
             OpenGitHubRepo = new Command(async () => await Browser.OpenAsync("https://github.com/braem/moodi"));
             OpenLicense = new Command(async () => await Browser.OpenAsync("https://github.com/braem/moodi/blob/master/LICENSE"));
             OpenThirdPartyPage = new Command(async () => await Shell.Current.GoToAsync(nameof(ThirdPartyPage)));
+            OpenGHProfile = new Command(async () => await Browser.OpenAsync("https://github.com/braem"));
+            OpenTwitterProfile = new Command(async () => await Browser.OpenAsync("https://twitter.com/braemie"));
         }
     }
 }
