9286fd347407fb4c26261714852e27c8da45e2a2
brae <brae@momentum-mod.org>
2021-08-10 20:36:54 -0700

    feat: switch font to Comfortaa
    muuuuuch better

--- moodi/moodi/ViewModels/ThirdPartyViewModel.cs
+++ moodi/moodi/ViewModels/ThirdPartyViewModel.cs
@@ -8,6 +8,7 @@ namespace moodi.ViewModels
         public Command OpenXamarinFormsRepo { get; }
         public Command OpenFFImageLoadingRepo { get; }
         public Command OpenCoreUIRepo { get; }
+        public Command OpenComfortaaLink { get; }
         public Command OpenSQLiteNetRepo { get; }
         public Command OpenSkiasharpRepo { get; }
 
@@ -16,6 +17,7 @@ namespace moodi.ViewModels
             OpenXamarinFormsRepo = new Command(async () => await Browser.OpenAsync("https://github.com/xamarin/Xamarin.Forms"));
             OpenFFImageLoadingRepo = new Command(async () => await Browser.OpenAsync("https://github.com/luberda-molinet/FFImageLoading"));
             OpenCoreUIRepo = new Command(async () => await Browser.OpenAsync("https://github.com/coreui/coreui-icons"));
+            OpenComfortaaLink = new Command(async () => await Browser.OpenAsync("https://www.fontsquirrel.com/fonts/Comfortaa"));
             OpenSQLiteNetRepo = new Command(async () => await Browser.OpenAsync("https://github.com/oysteinkrog/SQLite.Net-PCL"));
             OpenSkiasharpRepo = new Command(async () => await Browser.OpenAsync("https://github.com/mono/SkiaSharp"));
         }
