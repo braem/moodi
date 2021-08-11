a49d7631ca82fdaee4166c8202b69597fb5a1f8b
brae <brae@momentum-mod.org>
2021-08-10 20:36:54 -0700

    feat: switch font to Comfortaa
    muuuuuch better

--- moodi/moodi/AssemblyInfo.cs
+++ moodi/moodi/AssemblyInfo.cs
@@ -1,4 +1,9 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-[assembly: XamlCompilation(XamlCompilationOptions.Compile), ExportFont("Surface_Medium.otf", Alias = "Surface-Medium")]
+[assembly: XamlCompilation(XamlCompilationOptions.Compile),
+    ExportFont("Comfortaa-Bold.ttf", Alias = "Comfortaa-Bold"),
+    ExportFont("Comfortaa-Light.ttf", Alias = "Comfortaa-Light"),
+    ExportFont("Comfortaa-Medium.ttf", Alias = "Comfortaa-Medium"),
+    ExportFont("Comfortaa-Regular.ttf", Alias = "Comfortaa-Regular"),
+    ExportFont("Comfortaa-SemiBold.ttf", Alias = "Comfortaa-SemiBold")]
