44611169651c651b8239f5965a67199b4d203214
brae <brae@momentum-mod.org>
2021-08-21 10:19:22 -0700

    refactor: rename database
    this database can be used for things other than mood entries

--- moodi/moodi/App.xaml.cs
+++ moodi/moodi/App.xaml.cs
@@ -7,15 +7,15 @@ namespace moodi
 {
     public partial class App : Application
     {
-        private static MoodEntryDatabase _database;
-        public static MoodEntryDatabase Database { get { return _database; } }
+        private static Database _database;
+        public static Database Database { get { return _database; } }
 
         public App()
         {
             InitializeComponent();
             if (_database == null)
             {
-                _database = new MoodEntryDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "MoodEntries.db3"));
+                _database = new Database(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "MoodEntries.db3"));
             }
             MoodImageStore.Init();
 
